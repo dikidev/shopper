530291e383190721dbf782b41a15be66ef5ddc34
D <d@diki.dev>
2020-10-23 13:47:30 +0100

    Active Tab - items & Cart
    Creating the active tab - this helps keep track of the active tab and enable switching between the ItemsPage and the CartPage.
    
    - since useState returns an array we can use array destructuring
    -activeTab is initially set to the string 'items'
    -setActiveTab lets us update the activeTab state
    -we can choose any name we want for the variable and function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import "./App.css";
  
 const App = () => {
+  const [activeTab, setActiveTab] = useState("items");
+  
   return (
     <div className="App">
-      <Nav />
-      <main className="app-content">
-        <span>Empty</span>
+      <Nav activeTab={activeTab} onTabChange={setActiveTab} />
+      <main className="App-content">
+      <Content tab={activeTab}/>
       </main>
     </div>
   );
+};
+
+const Content = ({ tab }) => {
+  switch (tab) {
+    default:
+      case "items":
+      return <span>the items</span>;
+      case "cart":
+        return <span>the cart</span>;
+  }
 };
  
 export default App;
